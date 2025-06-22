2dd475282f059651a70f0463fb07b2eb537b49d6
Thomas Soares <thomas__soares@hotmail.com>
2025-06-22 08:32:02 -0300

    feat: add dayjs and next-safe-action dependencies; implement upsert doctor functionality with validation

---
+++ src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
+import { doctorsTable } from "@/db/schema";
+
+import UpsertDoctorForm from "./upsert-doctor-form";
+
+interface DoctorCardProps {
+  doctor: typeof doctorsTable.$inferSelect;
+}
+
+const DoctorCard = ({ doctor }: DoctorCardProps) => {
+  const [isUpsertDoctorDialogOpen, setIsUpsertDoctorDialogOpen] =
+    useState(false);
+
+  const doctorInitials = doctor.name
+    .split(" ")
+    .map((name) => name[0])
+    .join("");
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center gap-2">
+          <Avatar className="h-10 w-10">
+            <AvatarFallback>{doctorInitials}</AvatarFallback>
+          </Avatar>
+          <div>
+            <h3 className="text-sm font-medium">{doctor.name}</h3>
+            <p className="text-muted-foreground text-sm">{doctor.specialty}</p>
+          </div>
+        </div>
+      </CardHeader>
+      <Separator />
+      <CardContent className="flex flex-col gap-2"></CardContent>
+      <Separator />
+      <CardFooter className="flex flex-col gap-2">
+        <Dialog
+          open={isUpsertDoctorDialogOpen}
+          onOpenChange={setIsUpsertDoctorDialogOpen}
+        >
+          <DialogTrigger asChild>
+            <Button className="w-full">Ver detalhes</Button>
+          </DialogTrigger>
+          <UpsertDoctorForm
+            doctor={{
+              ...doctor,
+            }}
+            onSuccess={() => setIsUpsertDoctorDialogOpen(false)}
+          />
+        </Dialog>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default DoctorCard;
