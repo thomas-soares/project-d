f52c60d9013a5e4c81c23960d4d432b00aa6a8ae
Thomas Soares <thomas__soares@hotmail.com>
2025-08-16 23:44:28 -0300

    refactor: clean up createClinic function for improved readability and maintainability

--- src/actions/create-clinic/index.ts
+++ src/actions/create-clinic/index.ts
@@ -11,18 +11,10 @@ export const createClinic = async (name: string) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
   if (!session?.user) {
     throw new Error("Unauthorized");
   }
-
-  const [clinic] = await db
-    .insert(clinicsTable)
-    .values({
-      name,
-    })
-    .returning();
-
+  const [clinic] = await db.insert(clinicsTable).values({ name }).returning();
   await db.insert(usersToClinicsTable).values({
     userId: session.user.id,
     clinicId: clinic.id,
